a568ddd7ed661e82beb2fefbcb5a9e08ac566550
Oleg Kurilo <xolegx04@mail.ru>
2026-05-14 13:17:31 +0300

    Load React Flow styles in widget creator

--- src/widgets/WidgetCreator/WidgetCreator.tsx
+++ src/widgets/WidgetCreator/WidgetCreator.tsx
@@ -2,6 +2,7 @@ import { ReactFlowProvider } from "@xyflow/react";
 import { Editor } from "../../app/editor2";
 import type { WidgetCreatorProps } from "./types";
 import { Wrapper } from "./styled";
+import '@xyflow/react/dist/style.css';
 
 export function WidgetCreator({ onSave }: WidgetCreatorProps) {
   return (
